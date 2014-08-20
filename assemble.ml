29b685ce5c68b571c4beffdd33b07ab67f5a0a52
Thomas Gazagnaire <thomas@gazagnaire.org>
2014-08-20 14:39:01 +0100

    Update Makefile to latest Assemblage

--- assemble.ml
+++ assemble.ml
@@ -7,9 +7,9 @@ let ocaml_version =
   Scanf.sscanf Sys.ocaml_version "%d.%d" (fun i j -> i,j)
 
 let bin = bin "ocaml-dumpast" ~deps:[bytecomp; unix] (`Units [
-    (if ocaml_version < (4,2) then unit "parse_compat" (`Dir "src/401")
-     else unit "parse_compat" (`Dir "src/402"));
-    unit "dumpast" (`Dir "src")
+    (if ocaml_version < (4,2) then unit "parse_compat" (`Path ["src";"401"])
+     else unit "parse_compat" (`Path ["src";"402"]));
+    unit "dumpast" (`Path ["src"])
   ])
 
 let t = project "dumpast" [ bin ]
